e1953fd777f6f083668ee35543654e8dbd3977bd
aditiyax <aditya11austin@gmail.com>
2025-08-03 03:03:04 +0530

    added template code

--- dist/index.js
+++ dist/index.js
@@ -15,21 +15,57 @@ var __asyncValues = (this && this.__asyncValues) || function (o) {
     function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
     function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
 const genai_1 = require("@google/genai");
 const prompts_1 = require("./prompts");
-const apiKey = process.env.GEMINI_API_KEY;
-if (!apiKey) {
-    throw new Error("GEMINI_API_KEY environment variable not set.");
-}
-const genAI = new genai_1.GoogleGenAI({ apiKey });
+const express_1 = __importDefault(require("express"));
+const react_1 = require("./defaults/react");
+const node_1 = require("./defaults/node");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const ai = new genai_1.GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const prompt = req.body.prompt;
+    const response = yield ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: prompt,
+        config: {
+            systemInstruction: "Analyze the content prompt, and Return only a single word, either node or react or notSupported, based on what you think this project should be ",
+            maxOutputTokens: 500,
+        },
+    });
+    const answer = response.text; //react or node
+    if (answer == "react") {
+        console.log("REACT project received");
+        res.json({
+            prompts: [prompts_1.REACT_BLRPT_PROMPT, react_1.reactBasePrompt],
+        });
+        return;
+    }
+    else if (answer == "node") {
+        console.log("NODE project received");
+        res.json({
+            prompts: [node_1.nodeBasePrompt],
+        });
+        return;
+    }
+    else {
+        console.log("NOT_SUPPORTED received");
+        res.status(403).json({
+            message: "Code Language Not Supported, Pls provide 'react' or 'node' code! ",
+        });
+        return;
+    }
+}));
+app.listen(3000);
+console.log(`Server Running on {http://localhost:3000}`);
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         var _a, e_1, _b, _c;
-        // Make sure to include the following import:
-        // import {GoogleGenAI} from '@google/genai';
-        const ai = new genai_1.GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
         const response = yield ai.models.generateContentStream({
             model: "gemini-2.5-flash",
             contents: "Write a story about a magic backpack.",
@@ -57,4 +93,4 @@ function main() {
         }
     });
 }
-main();
+// main();
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,54 @@
 require("dotenv").config();
 import { GoogleGenAI } from "@google/genai";
-import { getSystemPrompt } from "./prompts";
+import { getSystemPrompt, REACT_BLRPT_PROMPT } from "./prompts";
+import express from "express";
+import { reactBasePrompt } from "./defaults/react";
+import { nodeBasePrompt } from "./defaults/node";
 
-const apiKey = process.env.GEMINI_API_KEY;
-if (!apiKey) {
-  throw new Error("GEMINI_API_KEY environment variable not set.");
-}
+const app = express();
+app.use(express.json());
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
-const genAI = new GoogleGenAI({ apiKey });
+app.post("/template", async (req, res) => {
+  const prompt = req.body.prompt;
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt,
+    config: {
+      systemInstruction:
+        "Analyze the content prompt, and Return only a single word, either node or react or notSupported, based on what you think this project should be ",
+      maxOutputTokens: 500,
+    },
+  });
+
+  const answer = response.text; //react or node
+  if (answer == "react") {
+    console.log("REACT project received");
+    res.json({
+      prompts: [REACT_BLRPT_PROMPT, reactBasePrompt],
+    });
+    return;
+  } else if (answer == "node") {
+    console.log("NODE project received");
+    res.json({
+      prompts: [nodeBasePrompt],
+    });
+    return;
+  } else {
+    console.log("NOT_SUPPORTED received");
+    res.status(403).json({
+      message:
+        "Code Language Not Supported, Pls provide 'react' or 'node' code! ",
+    });
+    return;
+  }
+});
+
+app.listen(3000);
+console.log(`Server Running on 'http://localhost:3000' `);
 
 async function main() {
-  // Make sure to include the following import:
-  // import {GoogleGenAI} from '@google/genai';
-  const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-
   const response = await ai.models.generateContentStream({
     model: "gemini-2.5-flash",
     contents: "Write a story about a magic backpack.",
@@ -29,4 +64,4 @@ async function main() {
   }
 }
 
-main();
+// main();
